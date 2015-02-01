7f6c57e68507ff05dacf51d16b38a3055d9c6651
Nathan Hoffmann <nhoffman@mines.edu>
2015-02-01 16:22:23 -0700

    Added a pair of ears

--- src/Cat.java
+++ src/Cat.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class Cat {
-	// drawing constants are private - noone needs to know what we're doing
+	// drawing constants are private - no one needs to know what we're doing
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 100;
 	// eyes will be about 1/4 from top of head and 1/4 from left
@@ -19,6 +19,13 @@ public class Cat {
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	//
+	private static final int EAR_HEIGHT = HEAD_DIMENSION/2;
+	private static final int EAR_Y1 = HEAD_DIMENSION;
+	private static final int EAR_Y2 = HEAD_DIMENSION/4;
+	private static final int EAR_X1 = HEAD_DIMENSION;
+	private static final int EAR_X2 = HEAD_DIMENSION/7*3;
+	
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -29,6 +36,14 @@ public class Cat {
 		// Draw the head
 		g2.setColor(Color.black);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
+		// Draw Ears
+		int[] earX = {EAR_X1, HEAD_DIMENSION - EAR_X2, HEAD_DIMENSION - EAR_X2};
+		int[] earY = {EAR_Y1, EAR_Y1, EAR_Y2 };
+		g2.setColor(Color.black);
+		g2.fillPolygon(earX, earY, 3);
+		earX[1] = EAR_X2 + HEAD_DIMENSION;
+		earX[2] = earX[1];
+		g2.fillPolygon(earX, earY, 3);
 		// Draw the eyes
 		g2.setColor(Color.red);
 		x = catX + EYE_X; 
@@ -42,6 +57,9 @@ public class Cat {
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
+
+
+		
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -8,7 +8,7 @@ public class MyDrawing extends JPanel {
 	private Cat cat;
 	public MyDrawing() 
 	{
-		// Add critters to the drawingg
+		// Add critters to the drawing
 		cat = new Cat();
 	}
 	// paintComponent is called automatically when the frame needs
