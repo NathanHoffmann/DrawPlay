088ccee169136b95baac97681ee421a6c1f6fdb4
Nathan Hoffmann <nhoffman@mymail.mines.edu>
2015-02-01 16:48:45 -0700

    Added Whiskers

--- src/Cat.java
+++ src/Cat.java
@@ -20,12 +20,16 @@ public class Cat {
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	//
-	private static final int EAR_HEIGHT = HEAD_DIMENSION/2;
 	private static final int EAR_Y1 = HEAD_DIMENSION;
 	private static final int EAR_Y2 = HEAD_DIMENSION/4;
 	private static final int EAR_X1 = HEAD_DIMENSION;
 	private static final int EAR_X2 = HEAD_DIMENSION/7*3;
-	
+	private static final int WHISK_X1 = HEAD_DIMENSION/5;
+	private static final int WHISK_X2 = HEAD_DIMENSION/5*2;
+	private static final int WHISK_Y1 = MOUTH_Y+HEAD_DIMENSION/2;
+	private static final int WHISK_Y2 = MOUTH_Y+HEAD_DIMENSION/3;
+	private static final int WHISK_Y3 = MOUTH_Y+HEAD_DIMENSION*2/3;
+
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -57,8 +61,14 @@ public class Cat {
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
-
-
+		// Draw whiskers
+		g2.setColor(Color.darkGray);
+		g2.drawLine(HEAD_DIMENSION+WHISK_X1, catY + MOUTH_Y, HEAD_DIMENSION+WHISK_X2, WHISK_Y1);
+		g2.drawLine(HEAD_DIMENSION+WHISK_X1, catY + MOUTH_Y, HEAD_DIMENSION+WHISK_X2, WHISK_Y2);
+		g2.drawLine(HEAD_DIMENSION+WHISK_X1, catY + MOUTH_Y, HEAD_DIMENSION+WHISK_X2, WHISK_Y3);
+		g2.drawLine(HEAD_DIMENSION-WHISK_X1, catY + MOUTH_Y, HEAD_DIMENSION-WHISK_X2, WHISK_Y1);
+		g2.drawLine(HEAD_DIMENSION-WHISK_X1, catY + MOUTH_Y, HEAD_DIMENSION-WHISK_X2, WHISK_Y2);
+		g2.drawLine(HEAD_DIMENSION-WHISK_X1, catY + MOUTH_Y, HEAD_DIMENSION-WHISK_X2, WHISK_Y3);
 		
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
